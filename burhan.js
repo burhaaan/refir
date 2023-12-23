3c189eef805b604df7d4839e3ce0b2c13ea7a6ca
Burhan Babar <51114605+burhaaan@users.noreply.github.com>
2023-12-23 01:35:48 +0100

    Update burhan.js

--- burhan.js
+++ burhan.js
@@ -1,4 +1,23 @@
-function addTwoNumbers(x,y) {
-  console.log('i am here')
-  return x+y
+const admin = require('firebase-admin')
+
+function addReferral(referralLink, email) {
+  const referralsRef = admin.firestore().collection('Referrals')
+
+  // Add a new document with programName and subdomain
+  referralsRef
+    .add({
+      referralLink: referralLink,
+      email: email,
+    })
+    .then((docRef) => {
+      console.log('Document written with ID:', docRef.id)
+      res.status(200).json({
+        success: true,
+        message: 'Referral stored successfully',
+      })
+    })
+    .catch((error) => {
+      console.error('Error adding referral:', error)
+      res.status(500).json({ error: 'Internal server error' })
+    })
 }
